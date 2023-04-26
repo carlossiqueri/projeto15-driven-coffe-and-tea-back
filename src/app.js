0c7f101c637b9bc224b8b7ebff56dc77b62afe2d
Carlos Siqueri <carlossiqueri@Carlos>
2023-04-26 16:19:55 -0300

    setup: first commit e inicialização do server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express  from "express";
+import cors from "cors";
+
+// Server configs
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
